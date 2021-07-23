8f95dddef85efa5e13de7d2dc019e9f7cca57662
Ronald Gutierrez Arratia <73799699+121-rrg@users.noreply.github.com>
2021-07-23 09:59:04 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "LinkedList.h"
+
 #include "iterator.h"
 using namespace std;
 
@@ -14,24 +14,11 @@ int main() {
     l1.insert(3,3);
     l1.insert(4,4);
     l1.insert(5,5);
-    l1.insert(100,5);
-    cout<<l1;
-    cout<<"Lista de caracteres"<<endl;
-    l2.insert('a',0);
-    l2.insert('b',1);
-    l2.insert('c',3);
-    l2.insert('d',2);
-    cout<<l2;
 
     Iterator<int> it;
-    for(it = l1.begin(); it != nullptr ; ++it)
+    for(it = l1.begin(); it != nullptr; ++it){
         cout<<it;
-    
-    /*
-    Iterator<int> it;
-    while(it != l1.end()){
-        cout<<it<<endl;
     }
-    */
+    
     return 0;
 }
